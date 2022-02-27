592d3add00262a1a82fa92571d9b9bc598bde940
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-02-27 13:23:37 +0100

    Create pobranie_ngrok.sh
    Pobranie i rozpakowanie ngroka

---
+++ pobranie_ngrok.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+sudo tar zxvf ngrok-stable-linux-amd64.tgz
+sudo rm ngrok-stable-linux-amd64.tgz
+echo "Ngrok został pobrany i rozpakowany w folderze, a plik instalacyjny został skasowany."
