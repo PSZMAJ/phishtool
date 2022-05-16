82a49a03ac476acd48d0d09ac3815f4136c4b3b7
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-05-16 14:47:55 +0200

    update v2

--- pobranie_ngrok.sh
+++ pobranie_ngrok.sh
@@ -3,3 +3,8 @@ wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 sudo tar zxvf ngrok-v3-stable-linux-amd64.tgz
 sudo rm ngrok-v3-stable-linux-amd64.tgz
 echo "Ngrok został pobrany i rozpakowany w folderze, a plik instalacyjny został skasowany."
+echo " podaj klucz z do powizania z Twoim kontem"
+read klucz
+./ngrok config add-authtoken $klucz
+echo " klucz zosta dodany " $klucz
+echo "koncze prace"
